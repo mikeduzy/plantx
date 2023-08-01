6529236bb44f9b2d749f5dc1894259494b2b04bf
Maltecius <133878175+Maltecius@users.noreply.github.com>
2023-08-01 22:43:58 +0200

    feature/navbar (does'nt work)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import  {Navbar} from './components/navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-import { Testcomponent} from './components/testcomponent';
 
 function App() {
   return (
     
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <Router>
 
-          Büsra war hier!
-
-        Hey friends!!!
-
-        </p>
-        <Testcomponent/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      
+      </Router>
     </div>
   );
 }
